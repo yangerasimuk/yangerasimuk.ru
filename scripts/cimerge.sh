cb4e6d9d29a2150d5c27e613c189aeeb4bf886ce
Ян Герасимук <yan.gerasimuk@yandex.ru>
2018-08-30 02:48:08 +0300

    Test 13

--- scripts/cimerge.sh
+++ scripts/cimerge.sh
@@ -2,14 +2,24 @@
 
 set -e # halt script on error
 
-git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git checkout --track origin/master
+git checkout --track origin/dev
 
-for branch in $(git branch -r|grep -v HEAD) ; do
-	echo branch
-    git checkout ${branch#origin/}
-    echo ${branch#origin/}
-done
+git fetch --all
 
 git checkout master
-git checkout dev
 git merge dev
+
+
+#git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+
+
+#for branch in $(git branch -r|grep -v HEAD) ; do
+#	echo branch
+#    git checkout ${branch#origin/}
+#    echo ${branch#origin/}
+#done
+
+#git checkout master
+#git checkout dev
+#git merge dev
