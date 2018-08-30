aa0337a378a0d818fd1c442cad55c29f038f293c
Ян Герасимук <yan.gerasimuk@yandex.ru>
2018-08-30 12:06:10 +0300

    Test 20

--- scripts/cipullrequest.sh
+++ scripts/cipullrequest.sh
@@ -1,7 +1,19 @@
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
 
-curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
--d "{\"body\": \"Hello world\"}" \
-"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
-
+if [ "$TRAVIS_BRANCH" != "dev" ]; then 
+    exit 0;
 fi
+export GIT_COMMITTER_EMAIL="yan.gerasimuk@yandex.ru"
+export GIT_COMMITTER_NAME="Ян Герасимук via Travis"
+git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
+git fetch --all || exit
+git checkout master || exit
+git merge --no-ff "$TRAVIS_COMMIT" || exit
+git push @github.com/">https://${GITHUB_TOKEN}@github.com/yangerasimuk/yangerasimuk.ru.git
+
+#if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+
+#curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+#-d "{\"body\": \"Hello world\"}" \
+#"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+
+#fi
