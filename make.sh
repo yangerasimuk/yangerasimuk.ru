e91754479f13caa3bec3b2d4842584f295d71f87
Ян Герасимук <yan.gerasimuk@yandex.ru>
2018-09-14 23:41:54 +0300

    Edit make.sh

--- make.sh
+++ make.sh
@@ -2,7 +2,12 @@
 
 set -e # halt script on error
 
-git branch -a | grep \* | cut -d ' ' -f2
+branch=$(git branch -a | grep \* | cut -d ' ' -f2)
+if [ branch != "dev" ]
+then
+echo "Current git branch is not dev"
+exit
+fi
 
 echo "> bundle exec jekyll build"
 bundle exec jekyll build
@@ -14,17 +19,14 @@ echo "> git add ."
 git add .
 
 date=$(date +%Y-%m-%d)
-echo "> git commit -m 'Rebuild site at "+date+"'"
-git commit -m "Build site at "+date
+echo "> git commit -m 'Rebuild site at "+$date+"'"
+git commit -m "Build site at "+$date
 
 echo "> git push origin"
 git push --force origin
 
-echo "> git checkout master"
-git checkout master
-
 echo "> git merge -s ours dev"
-git merge -s ours dev
+git merge -s ours master
 
 echo "> git merge dev"
 git merge dev
