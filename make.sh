d0a73b1b66ccc2cf3b1b9640da1c29726631df25
Ян Герасимук <yan.gerasimuk@yandex.ru>
2018-09-14 23:31:54 +0300

    Build site at +date

--- make.sh
+++ make.sh
@@ -2,25 +2,29 @@
 
 set -e # halt script on error
 
+git branch -a | grep \* | cut -d ' ' -f2
+
 echo "> bundle exec jekyll build"
 bundle exec jekyll build
 
-echo ""
 echo "> bundle exec htmlproofer ./docs"
 #bundle exec htmlproofer ./docs
 
 echo "> git add ."
 git add .
 
-DATE=`date +%Y-%m-%d`
-echo "> git commit -m 'Rebuild site at "+DATE+"'"
-git commit -m "Build site at "+DATE
+date=$(date +%Y-%m-%d)
+echo "> git commit -m 'Rebuild site at "+date+"'"
+git commit -m "Build site at "+date
 
 echo "> git push origin"
 git push --force origin
 
-echo "git checkout master"
+echo "> git checkout master"
 git checkout master
+
+echo "> git merge -s ours dev"
+git merge -s ours dev
 
 echo "> git merge dev"
 git merge dev
