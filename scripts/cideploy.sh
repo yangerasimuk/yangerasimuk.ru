ce9ce62ac87b102502292a1a2cb5385f0e3baaf2
Ян Герасимук <yan.gerasimuk@yandex.ru>
2018-09-14 20:50:23 +0300

    Experiment #7

--- scripts/cideploy.sh
+++ scripts/cideploy.sh
@@ -15,25 +15,28 @@ ls -la --group-directories-first
 export GIT_COMMITTER_EMAIL="yan.gerasimuk@yandex.ru"
 export GIT_COMMITTER_NAME="Ян Герасимук via Travis"
 
-git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
-git fetch --all || exit
+#git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
+#git fetch --all || exit
 
-git checkout dev
-git branch | grep \* | cut -d ' ' -f2
-git pull origin
+#git checkout dev
+#git branch | grep \* | cut -d ' ' -f2
+#git pull origin
 
 #git stash
-git checkout master
-git pull origin
-
-git checkout dev
-
-git merge -s ours master
-git checkout master
-git stash
-#git merge --no-ff "$TRAVIS_COMMIT" || exit
-git merge dev
+git checkout master || exit
+git merge "$TRAVIS_COMMIT" || exit
 git push "https://$GITHUB_TOKEN@github.com/yangerasimuk/yangerasimuk.ru.git"
-git stash pop
+
+#git pull origin
+
+#git checkout dev
+
+#git merge -s ours master
+#git checkout master
+#git stash
+#git merge --no-ff "$TRAVIS_COMMIT" || exit
+#git merge dev
+#git push "https://$GITHUB_TOKEN@github.com/yangerasimuk/yangerasimuk.ru.git"
+#git stash pop
 
 #git stash pop
