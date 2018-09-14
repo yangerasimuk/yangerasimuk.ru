922379f369126cc84f1a69d89d24d3e5b57ff900
Ян Герасимук <yan.gerasimuk@yandex.ru>
2018-09-14 20:03:43 +0300

    Try, try, try...

--- scripts/cideploy.sh
+++ scripts/cideploy.sh
@@ -14,13 +14,13 @@ export GIT_COMMITTER_EMAIL="yan.gerasimuk@yandex.ru"
 export GIT_COMMITTER_NAME="Ян Герасимук via Travis"
 
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
-git fetch --all || exit
+#git fetch --all || exit
 
-git stash
+#git stash
 
-git checkout master || exit
+#git checkout master || exit
 #git merge --no-ff "$TRAVIS_COMMIT" || exit
-git merge dev
+#git merge dev
 git push "https://$GITHUB_TOKEN@github.com/yangerasimuk/yangerasimuk.ru.git"
 
-git stash pop
+#git stash pop
