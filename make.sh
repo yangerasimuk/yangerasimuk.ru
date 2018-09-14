c15864a5b3c7d6ae804beec9e62da0355482ad12
Ян Герасимук <yan.gerasimuk@yandex.ru>
2018-09-15 00:04:50 +0300

    Build site at 2018-09-15

--- make.sh
+++ make.sh
@@ -12,22 +12,24 @@ fi
 echo "> bundle exec jekyll build"
 bundle exec jekyll build
 
-echo ""
-echo "> bundle exec htmlproofer ./docs"
+#echo "> bundle exec htmlproofer ./docs"
 #bundle exec htmlproofer ./docs
 
 echo "> git add ."
 git add .
 
 date=$(date +%Y-%m-%d)
-echo "> git commit -m 'Rebuild site at "+$date+"'"
-git commit -m "Build site at "+$date
+echo "> git commit -m 'Rebuild site at $date'"
+git commit -m "Build site at $date"
 
 echo "> git push origin"
 git push --force origin
 
-echo "> git merge -s ours dev"
+echo "> git merge -s ours master"
 git merge -s ours master
+
+echo "> git checkout master"
+git checkout master
 
 echo "> git merge dev"
 git merge dev
